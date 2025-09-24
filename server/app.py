6f108c771b058555e7d121750268bf1d68be1bc4
Nick Weiland <njweiland23@gmail.com>
2025-09-24 11:03:42 -0500

    Finished frontend styling

--- server/app.py
+++ server/app.py
@@ -256,4 +256,4 @@ def internal_error(error):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5500, debug=True)
